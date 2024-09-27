33de535dfcf0b0dbb60c28a5ffe6006ce3099c05
miejejaw <mierafjejaw@gmail.com>
2024-09-27 17:23:10 +0300

    setup chat app project

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+
+	err := router.Run(":8080")
+	if err != nil {
+		return
+	}
+}
