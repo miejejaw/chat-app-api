28e616f188b9c9fe140e1385fec01d4e6007b38e
miejejaw <mierafjejaw@gmail.com>
2024-10-08 11:33:40 +0300

    implement search user by name and username

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -14,6 +14,7 @@ type UserRepository interface {
 	FindByUsername(username string) (*models.User, error)
 	IsUsernameExist(username string) bool
 	IsEmailExist(email string) bool
+	SearchUser(currentUsername string, searchContent string) ([]models.User, error)
 }
 
 type userRepository struct {
@@ -81,4 +82,34 @@ func (r *userRepository) IsEmailExist(email string) bool {
 		return false
 	}
 	return true
+}
+
+func (r *userRepository) SearchUser(currentUsername string, searchContent string) ([]models.User, error) {
+	var users []models.User
+
+	// Return empty list if no search content is provided
+	if searchContent == "" {
+		return users, nil
+	}
+
+	// Create a base query that excludes the current user's username
+	query := r.db.Where("username != ?", currentUsername)
+
+	// Search by username if searchContent starts with '@'
+	if searchContent[0] == '@' {
+		// Search for usernames starting with the specified content
+		if err := query.Where("username LIKE ?", "%"+searchContent[1:]+"%").Find(&users).Error; err != nil {
+			return nil, err
+		}
+	} else {
+		// Search by first name, last name, or username
+		if err := query.Where("first_name LIKE ?", "%"+searchContent+"%").
+			Or("last_name LIKE ?", "%"+searchContent+"%").
+			Or("username LIKE ?", "%"+searchContent+"%").
+			Find(&users).Error; err != nil {
+			return nil, err
+		}
+	}
+
+	return users, nil
 }
--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -7,12 +7,24 @@ import (
 	"fmt"
 )
 
+type SearchResponse struct {
+	Profile struct {
+		ID              uint   `json:"id"`
+		Username        string `json:"username"`
+		FirstName       string `json:"first_name"`
+		LastName        string `json:"last_name"`
+		ProfileImageUrl string `json:"profile_image_url"`
+	} `json:"profile"`
+	LastSeen string `json:"last_seen"`
+}
+
 type UserService interface {
 	CreateUser(user *models.User) (*models.User, error)
 	GetUserByID(id uint) (*models.User, error)
 	GetAllUsers() ([]models.User, error)
 	UpdateUser(user *models.User) (*models.User, error)
 	DeleteUser(id uint) error
+	SearchUser(currentUsername string, searchContent string) ([]SearchResponse, error)
 }
 
 type userService struct {
@@ -55,4 +67,29 @@ func (s *userService) UpdateUser(user *models.User) (*models.User, error) {
 
 func (s *userService) DeleteUser(id uint) error {
 	return s.userRepository.DeleteUser(id)
+}
+
+func (s *userService) SearchUser(currentUsername string, searchContent string) ([]SearchResponse, error) {
+	response, err := s.userRepository.SearchUser(currentUsername, searchContent)
+
+	var searchResponse []SearchResponse
+	for _, user := range response {
+		searchResponse = append(searchResponse, SearchResponse{
+			Profile: struct {
+				ID              uint   `json:"id"`
+				Username        string `json:"username"`
+				FirstName       string `json:"first_name"`
+				LastName        string `json:"last_name"`
+				ProfileImageUrl string `json:"profile_image_url"`
+			}{
+				ID:              user.ID,
+				Username:        user.Username,
+				FirstName:       user.FirstName,
+				LastName:        user.LastName,
+				ProfileImageUrl: user.ProfileImageUrl,
+			},
+			LastSeen: "2021-09-01 15:04",
+		})
+	}
+	return searchResponse, err
 }
