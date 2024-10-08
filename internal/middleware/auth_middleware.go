ac1488b81710cd2f5bbb433ad3e40c5d5bf5a27b
miejejaw <mierafjejaw@gmail.com>
2024-10-08 11:36:09 +0300

    add middleware for authentication

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -1,30 +1,43 @@
-package middlewares
+package middleware
 
 import (
 	"chat-app-api/internal/utils"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
 			c.Abort()
 			return
 		}
 
+		// Check if the Authorization header is in the format "Bearer <token>"
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == authHeader {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header format must be 'Bearer <token>'"})
+			c.Abort()
+			return
+		}
+
+		// Parse and validate the access token
 		claims, err := utils.ParseAccessToken(tokenString)
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
 			c.Abort()
 			return
 		}
 
-		c.Set("user_claims", claims)
+		// Store user info in context for future use in handlers
+		c.Set("UserID", claims.UserID)
+		c.Set("Username", claims.Username)
+		c.Set("Email", claims.Email)
+
 		c.Next()
 	}
 }
